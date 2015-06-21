c8edea2fa37f6b3d2a78d29d056b03701efb073e
Vinny Tiwari <tiwari.vineet@outlook.com>
2015-06-21 15:18:48 -0700

    Initial Commit

---
+++ ninja/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ninja
+//
+//  Created by Vineet Tiwari on 2015-06-21.
+//  Copyright (c) 2015 vinny.co. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ninja/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  ninja
+//
+
+//  Copyright (c) 2015 vinny.co. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
